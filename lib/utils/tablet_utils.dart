c57c171a85bd3f40038f54f737dbb539be47794f
Haggai <haggaibb@gmail.com>
2025-11-26 18:29:49 +0200

    ----- dev 1,3 ----- Tablet view added
    -----
    dev 1,2
    -----
    
    -- TODO  need to add version management admin for weighted avg
    
    -- fixed the y-axis in Sakim chart so it is like Meshulash (inverted)
    
    -- add grid only use case for Sakim and Meshulash
    
    -- fixed save the kid AI report for future use
    
    -- added #.## to grades
    
    fixed - save button can not be reached by some phones.
    
    fiexed - removed await in alonka round, so there is no interbnet issue
    
    ------------ done -----------------
    
    -- sort fixes - list of instructors names dropdown in admin,
     list of events in Admin and Instructor
    
    -- fixed some sizing and spacing
    
    -- fixed alonka check of last round paricipants, when alonka starts it throws an error as last does not exist.
    
    - added guide on docs and linked to app bar , for each page link to relevant screenshot
    
    --------
    dev 1.1
    ---------
    
    -- TODO - save the report for future use
    
    -- TODO  need to add version management admin for weighted avg
    
    - added dropped to long press in comments page, and support for it in the relevant pages
    
    - added grades for testes in perofrmace page
    - fixing alonks GO to be viable only when prev round was finalized
    
    - disabled editing of cols in grades page, only instructor grade is enabled
    
    - fixed the Admin index, when del an event it del all events.
    
    - added version to menu in home page and build script
    
    - fixed p. ai report to work only on button click
    
    - double click to decrease round for participant - sakim and meshulash
    
    - added in Alonka an order by button, arrival or by order
    
    - fixed col size in grade page
    
    - finish weighted Avg task - move to cloud - moved

---
+++ lib/utils/tablet_utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Utility function to detect if the current device is a tablet
+/// Uses width > 600px as the threshold (standard Flutter tablet detection)
+bool isTablet(BuildContext context) {
+  return MediaQuery.of(context).size.width > 600;
+}
+
+/// Get scaled font size for tablets
+/// Returns fontSize * 1.3 for tablets, fontSize otherwise
+double getTabletScaledFontSize(BuildContext context, double fontSize) {
+  return isTablet(context) ? fontSize * 1.3 : fontSize;
+}
+
