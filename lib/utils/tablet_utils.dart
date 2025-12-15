42d51388e53bcea4ddce2ff77265d428bcf3cc14
Haggai <haggaibb@gmail.com>
2025-12-15 21:23:49 +0200

    ----- dev 1,3 ----- Tablet view added
    -----
    dev 1,2
    -----
    
    -- TODO  need to add version management admin for weighted avg
    
    -- fixed the y-axis in Sakim chart so it is like Meshulash (inverted)
    
    -- add grid only use case for Sakim and Meshulash
    
    -- fixed save the kid AI report for future use
    
    -- added #.## to grades
    
    fixed - save button can not be reached by some phones.
    
    fiexed - removed await in alonka round, so there is no interbnet issue
    
    ------------ done -----------------
    
    -- sort fixes - list of instructors names dropdown in admin,
     list of events in Admin and Instructor
    
    -- fixed some sizing and spacing
    
    -- fixed alonka check of last round paricipants, when alonka starts it throws an error as last does not exist.
    
    - added guide on docs and linked to app bar , for each page link to relevant screenshot
    
    --------
    dev 1.1
    ---------
    
    -- TODO - save the report for future use
    
    -- TODO  need to add version management admin for weighted avg
    
    - added dropped to long press in comments page, and support for it in the relevant pages
    
    - added grades for testes in perofrmace page
    - fixing alonks GO to be viable only when prev round was finalized
    
    - disabled editing of cols in grades page, only instructor grade is enabled
    
    - fixed the Admin index, when del an event it del all events.
    
    - added version to menu in home page and build script
    
    - fixed p. ai report to work only on button click
    
    - double click to decrease round for participant - sakim and meshulash
    
    - added in Alonka an order by button, arrival or by order
    
    - fixed col size in grade page
    
    - finish weighted Avg task - move to cloud - moved

--- lib/utils/tablet_utils.dart
+++ lib/utils/tablet_utils.dart
@@ -14,3 +14,4 @@ double getTabletScaledFontSize(BuildContext context, double fontSize) {
 
 
 
+
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,36 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import connectivity_plus
+import file_selector_macos
+import firebase_app_check
+import firebase_auth
+import firebase_core
+import firebase_storage
+import flutter_localization
+import package_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
+import wakelock_plus
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
