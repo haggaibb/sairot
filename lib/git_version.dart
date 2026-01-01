8be43b810657f87894e2c6495927a58818b31920
Haggai <haggaibb@gmail.com>
2026-01-01 13:47:27 +0200

    ---------------------- version: 1.0.0+8
    --- adding order of arrival grading to exercises
    
    ----------------------
    version: 1.0.0+6
    
    --  adding audio
    
    ----------------------
    version: 1.0.0+5
    
    --  Enabling Web
    
    ----------------------
    version: 1.0.0+4
    
    --- adding qualified recruits data when event finalized and aligning with admin
    
    ------------------------
    version: 1.0.0+3
    
    -- adding android id and manual device pairing.
    
    --- name change and store config to support config.
    ------------------
    dev 1,4
    -----------
    --added android id on log press of title in login
    
    --adding kiosk support
    
    -----
    dev 1,3
    -----
    Tablet view added
    
    -----
    dev 1,2
    -----
    
    -- TODO  need to add version management admin for weighted avg
    
    -- fixed the y-axis in Sakim chart so it is like Meshulash (inverted)
    
    -- add grid only use case for Sakim and Meshulash
    
    -- fixed save the kid AI report for future use
    
    -- added #.## to grades
    
    fixed - save button can not be reached by some phones.
    
    fiexed - removed await in alonka round, so there is no interbnet issue
    
    ------------ done -----------------
    
    -- sort fixes - list of instructors names dropdown in admin,
     list of events in Admin and Instructor
    
    -- fixed some sizing and spacing
    
    -- fixed alonka check of last round paricipants, when alonka starts it throws an error as last does not exist.
    
    - added guide on docs and linked to app bar , for each page link to relevant screenshot
    
    --------
    dev 1.1
    ---------
    
    -- TODO - save the report for future use
    
    -- TODO  need to add version management admin for weighted avg
    
    - added dropped to long press in comments page, and support for it in the relevant pages
    
    - added grades for testes in perofrmace page
    - fixing alonks GO to be viable only when prev round was finalized
    
    - disabled editing of cols in grades page, only instructor grade is enabled
    
    - fixed the Admin index, when del an event it del all events.
    
    - added version to menu in home page and build script
    
    - fixed p. ai report to work only on button click
    
    - double click to decrease round for participant - sakim and meshulash
    
    - added in Alonka an order by button, arrival or by order
    
    - fixed col size in grade page
    
    - finish weighted Avg task - move to cloud - moved

--- lib/git_version.dart
+++ lib/git_version.dart
@@ -1,3 +1,3 @@
 /// Auto-generated Git version and branch info
-const String gitVersion = '56c6baa';
-const String gitBranch = '1.0.0+7';
+const String gitVersion = '1e907d2';
+const String gitBranch = '1.0.0+8';
