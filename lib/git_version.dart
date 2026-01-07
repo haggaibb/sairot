2c972e08ee2472171c4baed3253cb30ac8ab8460
Haggai <haggaibb@gmail.com>
2026-01-08 01:29:54 +0200

    Update git version info

--- lib/git_version.dart
+++ lib/git_version.dart
@@ -1,3 +1,3 @@
 /// Auto-generated Git version and branch info
-const String gitVersion = 'da420c6';
-const String gitBranch = '1.0.0+10';
+const String gitVersion = 'c4ceebd';
+const String gitBranch = '1.0.0+11';
