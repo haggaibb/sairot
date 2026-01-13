b250d2bd034efa7cb54b520be37b134f4d8a737e
Haggai <haggaibb@gmail.com>
2026-01-13 15:18:13 +0200

    ---------------------- version: 1.0.0+14
    -- added cache cleanup (hive) 30 days cycle.
    
    -- fixed delEvent to only delete info of this app and not shifts
    
    -- making more fixes to have this work offline, for example loading open events.
    
    -- adding OCR offline using ML Kit and have fall back to vertexAI
    
    -- adding grading exercise option after an exercise is done.
    
    ----------------------
    version: 1.0.0+13
    
    -- fixing issue of generic comments in interview page, allowing to erase them and not just show them
    
    -- added a 3 tab info to the interview page
    
    --connected comments to AI to get a summary
    
    ----------------------
    version: 1.0.0+12
    
    -- improved grades page, loading indication, click and not double , blue highlight only on number click and added group strength to the app bar to affect grades
    
    -- added playground
    
    - fixed badge size for big fonts
    
    ----------------------
    version: 1.0.0+11
    
    -- fixed back position indicator and not order in exercise
    
    -- attempting to fix order of arrival and undo that creates bugs in graph
    
    -- fixed graphs in performance page
    
    ----------------------
    version: 1.0.0+10
    
    -- fixed many errors regrading web that came from moving to offline.
    
    ----------------------
    version: 1.0.0+9
    
    -- added matrix a new view to alonka exercise
    
    -- updated comments dialog to save without adding and also added multiline
    
    -- added swipe and arrows near the title in performance page
    
    -- made work offline from start
    
    -- added matrix to alonka graph
    
    -- cleaned app (old images, files etc)
    
    -- adding performance to the interview page,
    
    -- added comments indicator to grades page
    
    -- freezing the first col in the page grade
    
    ----------------------
    version: 1.0.0+8
    
    --- adding order of arrival grading to exercises
    
    ----------------------
    version: 1.0.0+6
    
    --  adding audio
    
    ----------------------
    version: 1.0.0+5
    
    --  Enabling Web
    
    ----------------------
    version: 1.0.0+4
    
    --- adding qualified recruits data when event finalized and aligning with admin
    
    ------------------------
    version: 1.0.0+3
    
    -- adding android id and manual device pairing.
    
    --- name change and store config to support config.
    ------------------
    dev 1,4
    -----------
    --added android id on log press of title in login
    
    --adding kiosk support
    
    -----
    dev 1,3
    -----
    Tablet view added
    
    -----
    dev 1,2
    -----
    
    -- TODO  need to add version management admin for weighted avg
    
    -- fixed the y-axis in Sakim chart so it is like Meshulash (inverted)
    
    -- add grid only use case for Sakim and Meshulash
    
    -- fixed save the kid AI report for future use
    
    -- added #.## to grades
    
    fixed - save button can not be reached by some phones.
    
    fiexed - removed await in alonka round, so there is no interbnet issue
    
    ------------ done -----------------
    
    -- sort fixes - list of instructors names dropdown in admin,
     list of events in Admin and Instructor
    
    -- fixed some sizing and spacing
    
    -- fixed alonka check of last round paricipants, when alonka starts it throws an error as last does not exist.
    
    - added guide on docs and linked to app bar , for each page link to relevant screenshot
    
    --------
    dev 1.1
    ---------
    
    -- TODO - save the report for future use
    
    -- TODO  need to add version management admin for weighted avg
    
    - added dropped to long press in comments page, and support for it in the relevant pages
    
    - added grades for testes in perofrmace page
    - fixing alonks GO to be viable only when prev round was finalized
    
    - disabled editing of cols in grades page, only instructor grade is enabled
    
    - fixed the Admin index, when del an event it del all events.
    
    - added version to menu in home page and build script
    
    - fixed p. ai report to work only on button click
    
    - double click to decrease round for participant - sakim and meshulash
    
    - added in Alonka an order by button, arrival or by order
    
    - fixed col size in grade page
    
    - finish weighted Avg task - move to cloud - moved

--- lib/git_version.dart
+++ lib/git_version.dart
@@ -1,3 +1,3 @@
 /// Auto-generated Git version and branch info
-const String gitVersion = '41bb588';
-const String gitBranch = '1.0.0+13';
+const String gitVersion = 'ddc6644';
+const String gitBranch = '1.0.0+14';
